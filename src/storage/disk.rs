372fe7b5f13f9185483f10ed96e135b73bd87013
Henock Dinberu <henockdinberu11@gmail.com>
2026-09-14 21:32:45 -0400

    feat: testing for storage engine

--- src/storage/disk.rs
+++ src/storage/disk.rs
@@ -85,4 +85,76 @@ impl DiskManager {
 
         Ok(new_page_id)
     }
+    // Should sync any in-memory data to be stored into disk (fsync)
+    pub fn sync(&mut self) -> std::io::Result<()> {
+        self.file.sync_all()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::OpenOptions;
+    use super::*;
+
+    struct TmpFile(std::path::PathBuf);
+
+    impl TmpFile {
+        fn new(name: &str) -> Self {
+            let path = std::env::temp_dir().join(format!("{name}.db"));
+            Self(path)
+        }
+    }
+
+    // Cleans up file from testing
+    impl Drop for TmpFile {
+        fn drop(&mut self) {
+            let _ = std::fs::remove_file(&self.0);
+        }
+    }
+
+    #[test]
+    fn test_read_write() -> std::io::Result<()> {
+        let test_file = TmpFile::new("db_test");
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(&test_file.0)?;
+
+        let mut dm = DiskManager::new(file, 0);
+
+        let page0_id = dm.allocate_page()?;
+        let page1_id = dm.allocate_page()?;
+
+        // Ensures page 0 and page 1 are allocated properly
+        assert_eq!(page0_id, 0);
+        assert_eq!(page1_id, 1);
+        assert_eq!(dm.num_pages, 2);
+
+        // Makes page buffers for data
+        let mut page0_data = [0u8; PAGE_SIZE];
+        let mut page1_data = [0u8; PAGE_SIZE];
+
+        // Stores text in first 10 bytes
+        page0_data[0..10].copy_from_slice("HELLOTHERE".as_bytes());
+        page1_data[0..11].copy_from_slice("HELLOTHERE2".as_bytes());
+
+        // Writes to both pages
+        dm.write_page(page0_id, &page0_data)?;
+        dm.write_page(page1_id, &page1_data)?;
+
+        // Imitates buffer mgr's cache requesting read using mutable buffers
+        let mut read_page0 = [0u8; PAGE_SIZE];
+        let mut read_page1 = [0u8; PAGE_SIZE];
+
+        // Reads data from both pages
+        dm.read_page(page0_id, &mut read_page0)?;
+        dm.read_page(page1_id, &mut read_page1)?;
+
+        assert_eq!(&read_page0[0..10], "HELLOTHERE".as_bytes());
+        assert_eq!(&read_page1[0..11], "HELLOTHERE2".as_bytes());
+
+        Ok(())
+    }
 }
