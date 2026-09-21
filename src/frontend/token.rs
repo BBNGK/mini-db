e9ee9a36dae0db5881f8561f8c7571778ceb04e1
F0ud <fuaadmaricar@gmail.com>
2026-09-20 22:55:09 -0400

    Stored Keywords and Operations/Symbols in Hashmap.

--- src/frontend/token.rs
+++ src/frontend/token.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 pub enum TokenType {
     Identifier,
     Number,
@@ -34,6 +35,14 @@ pub struct Token {
     pub literal: String,
 }
 
+// Struct to take the input string and tokenize it using a Hashmap to store keywords and operations
+pub struct Tokenizer {
+    input: String,
+    position: usize,
+    keyword: HashMap<String, TokenType>,
+    operation: HashMap<String, TokenType>,
+}
+
 impl Token {
     pub fn new(token_type: TokenType, literal: String) -> Token {
         Token {
@@ -50,7 +59,36 @@ impl Tokenizer {
 	// Taking the character at each 'position' and delegating it to the appropriate collector. 
 	// After all inputs are used, Return None.
 	fn new (input: String) -> Self {
-		Tokenizer{ input, position: 0 }
+        let keyword = HashMap::from([
+            ("SELECT".to_string(), TokenType::Select),
+            ("FROM".to_string(), TokenType::From),
+            ("WHERE".to_string(), TokenType::Where),
+            ("INSERT".to_string(), TokenType::Insert),
+            ("INTO".to_string(), TokenType::Into),
+            ("VALUES".to_string(), TokenType::Values),
+            ("CREATE".to_string(), TokenType::Create),
+            ("TABLE".to_string(), TokenType::Table),
+            ("DELETE".to_string(), TokenType::Delete),
+            ("UPDATE".to_string(), TokenType::Update),
+            ("SET".to_string(), TokenType::Set),
+            ("INT".to_string(), TokenType::Int),
+            ("AND".to_string(), TokenType::And),
+            ("OR".to_string(), TokenType::Or)
+        ]);
+        let operation = HashMap::from([
+            ("=".to_string(), TokenType::Equal),
+            ("!=".to_string(), TokenType::NotEqual),
+            ("<".to_string(), TokenType::LessThan),
+            (">".to_string(), TokenType::GreaterThan),
+            ("<=".to_string(), TokenType::LessThanEqual),
+            (">=".to_string(), TokenType::GreaterThanEqual),
+            ("*".to_string(), TokenType::Asterisk),
+            (",".to_string(), TokenType::Comma),
+            (";".to_string(), TokenType::Semicolon),
+            ("(".to_string(), TokenType::LeftParen),
+            (")".to_string(), TokenType::RightParen)
+        ]);
+		Tokenizer{ input, position: 0, keyword, operation }
 	}
 	fn get_next_token (&mut self) -> Option<Token> {
 		if self.position >= self.input.len() {
