8e40fc5f6fa39e08d2868c397716af3f98e87b51
Fuaad Maricar <fuaadmaricar@gmail.com>
2026-09-24 20:08:17 -0400

    Transferred code from token.rs to lexer.rs. token.rs contains the keywords enum. Lexer.rs contains the lex(tokenizer). Fixed functions for get_next_token() to loop through hashmaps to tokenize keywords and operations.

--- src/frontend/token.rs
+++ src/frontend/token.rs
@@ -1,5 +1,6 @@
-use std::collections::HashMap;
 
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum TokenType {
     Unknown,
     Identifier,
@@ -37,13 +38,6 @@ pub struct Token {
     pub literal: String,
 }
 
-// Struct to take the input string and tokenize it using a Hashmap to store keywords and operations
-pub struct Tokenizer {
-    input: String,
-    position: usize,
-    keyword: HashMap<String, TokenType>,
-    operation: HashMap<String, TokenType>,
-}
 
 impl Token {
     pub fn new(token_type: TokenType, literal: String) -> Token {
@@ -54,87 +48,3 @@ impl Token {
     }
 }
 
-impl Tokenizer {
-    // Taking the character at each 'position' and delegating it to the appropriate collector.
-    // After all inputs are used, Return None.
-    fn new(input: String) -> Self {
-        let keyword = HashMap::from([
-            ("SELECT".to_string(), TokenType::Select),
-            ("FROM".to_string(), TokenType::From),
-            ("WHERE".to_string(), TokenType::Where),
-            ("INSERT".to_string(), TokenType::Insert),
-            ("INTO".to_string(), TokenType::Into),
-            ("VALUES".to_string(), TokenType::Values),
-            ("CREATE".to_string(), TokenType::Create),
-            ("TABLE".to_string(), TokenType::Table),
-            ("DELETE".to_string(), TokenType::Delete),
-            ("UPDATE".to_string(), TokenType::Update),
-            ("SET".to_string(), TokenType::Set),
-            ("INT".to_string(), TokenType::Int),
-            ("AND".to_string(), TokenType::And),
-            ("OR".to_string(), TokenType::Or),
-        ]);
-        let operation = HashMap::from([
-            ("=".to_string(), TokenType::Equal),
-            ("!=".to_string(), TokenType::NotEqual),
-            ("<".to_string(), TokenType::LessThan),
-            (">".to_string(), TokenType::GreaterThan),
-            ("<=".to_string(), TokenType::LessThanEqual),
-            (">=".to_string(), TokenType::GreaterThanEqual),
-            ("*".to_string(), TokenType::Asterisk),
-            (",".to_string(), TokenType::Comma),
-            (";".to_string(), TokenType::Semicolon),
-            ("(".to_string(), TokenType::LeftParen),
-            (")".to_string(), TokenType::RightParen),
-        ]);
-        Tokenizer {
-            input,
-            position: 0,
-            keyword,
-            operation,
-        }
-    }
-    fn get_next_token(&mut self) -> Option<Token> {
-        if self.position >= self.input.len() {
-            return None;
-        }
-        // helpful for reading in larger inputs since Rust is bad with constant-time indexing for strings.
-        let current_character = self.input.chars().nth(self.position).unwrap();
-        if current_character.is_alphabetic() {
-            return self.collect_identifier();
-        } else {
-            self.position += 1;
-            return Some(Token::new(
-                TokenType::Unknown,
-                current_character.to_string(),
-            ));
-        }
-    }
-    // a run of alphabetic characters gets consumed as a token identifier
-    // Note: only alphabetic chars are read
-    fn collect_identifier(&mut self) -> Option<Token> {
-        let start_position = self.position;
-        while self.position < self.input.len()
-            && self
-                .input
-                .chars()
-                .nth(self.position)
-                .unwrap()
-                .is_alphabetic()
-        {
-            self.position += 1;
-        }
-        Some(Token::new(
-            TokenType::Identifier,
-            self.input[start_position..self.position].to_string(),
-        ))
-    }
-    // returns all the tokens in the tokenizer
-    fn tokenize(&mut self) -> Vec<Token> {
-        let mut tokens = Vec::new();
-        while let Some(token) = self.get_next_token() {
-            tokens.push(token);
-        }
-        tokens
-    }
-}
